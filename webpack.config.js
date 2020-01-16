c67db958749e73a8f526fcc6a87c62edde72aa71
andy <huayu.cao@pintec.com>
2020-01-16 16:54:12 +0800

    add webpack and image

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'file'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
